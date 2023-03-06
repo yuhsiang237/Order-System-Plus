f1d68fa353de118db646229944f3aef68352d3e7
Yu Hsiang <umworksite@gmail.com>
2023-03-06 16:21:25 +0800

    Adjust response, add Id to Model #12

--- OrderSystemPlus/OrderSystemPlus/Models/BusinessActor/_UserManage/Queries/RspGetUserInfo.cs
+++ OrderSystemPlus/OrderSystemPlus/Models/BusinessActor/_UserManage/Queries/RspGetUserInfo.cs
@@ -2,6 +2,7 @@
 {
     public class RspGetUserInfo
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Account { get; set; }
--- OrderSystemPlus/OrderSystemPlus/Models/BusinessActor/_UserManage/Queries/RspGetUserList.cs
+++ OrderSystemPlus/OrderSystemPlus/Models/BusinessActor/_UserManage/Queries/RspGetUserList.cs
@@ -2,6 +2,7 @@
 {
     public class RspGetUserList
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Account { get; set; }
