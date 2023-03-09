6b7132bc8c21a9cf10442a6188aa63630a8ace4a
Yu Hsiang <umworksite@gmail.com>
2023-03-09 10:41:24 +0800

    Add Product DA #10

---
+++ OrderSystemPlus/OrderSystemPlus/DataAccessor/_Product/Queries/ProductQuery.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+
+using Dapper;
+
+using OrderSystemPlus.Models;
+using OrderSystemPlus.Models.DataAccessor.Queries;
+
+namespace OrderSystemPlus.DataAccessor.Queries
+{
+    public class ProductQuery : IProductQuery
+    {
+        public async Task<List<ProductQueryModel>> FindByOptionsAsync(int? id = null, string? name = null)
+        {
+            string sql = @"
+                           SELECT
+                              [Id]
+                              ,[Number]
+                              ,[Name]
+                              ,[Price]
+                              ,[Description]
+                              ,[CurrentUnit]
+                              ,[CreatedOn]
+                              ,[UpdatedOn]
+                              ,[IsValid]
+                           FROM [dbo].[Product]";
+
+            var conditions = new List<string>
+            {
+                "[IsValid] = @IsValid",
+            };
+            if (id.HasValue)
+                conditions.Add("[Id] = @Id");
+            if (!string.IsNullOrEmpty(name))
+                conditions.Add("[Name] LIKE @Name");
+
+            if (conditions.Any())
+                sql = string.Concat(sql, $" WHERE {string.Join(" AND ", conditions)}");
+
+            var result = default(List<ProductQueryModel>);
+            using (SqlConnection conn = new SqlConnection(DBConnection.GetConnectionString()))
+            {
+                result = (await conn.QueryAsync<ProductQueryModel>(sql, new
+                {
+                    IsValid = true,
+                    Name = '%' + name + '%',
+                    Id = id,
+                })).ToList();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ OrderSystemPlus/OrderSystemPlus/DataAccessor/_Product/Queries/_/IProductQuery.cs
@@ -0,0 +1,15 @@
+﻿using OrderSystemPlus.Models.DataAccessor.Queries;
+
+namespace OrderSystemPlus.DataAccessor.Queries
+{
+    public interface IProductQuery
+    {
+        /// <summary>
+        /// 查詢Product資料們
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Task<List<ProductQueryModel>> FindByOptionsAsync(int? id,string? name);
+    }
+}
