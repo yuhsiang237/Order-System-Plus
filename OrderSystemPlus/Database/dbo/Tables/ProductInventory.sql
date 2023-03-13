5f50be4a70a3fd6cdaecccb5c0ed4c64987b7245
Yu Hsiang <umworksite@gmail.com>
2023-03-13 15:17:06 +0800

    Adjust inventory table

--- OrderSystemPlus/Database/dbo/Tables/ProductInventory.sql
+++ OrderSystemPlus/Database/dbo/Tables/ProductInventory.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[ProductInventory] (
     [Id]          INT             IDENTITY (1, 1) NOT NULL,
     [ProductId]   INT             NULL,
-    [Unit]        DECIMAL (18, 4) NULL,
+    [Quantity]        DECIMAL (18, 4) NULL,
     [Description] VARCHAR (500)   NULL,
-    [CreatedAt]   DATETIME2 (7)   CONSTRAINT [DF_ProductInventory_created_at] DEFAULT (getdate()) NULL,
+    [CreatedOn]  DATETIME   NOT NULL,
+    [UpdatedOn] DATETIME   NOT NULL,
+    [IsValid] BIT            NOT NULL,
     CONSTRAINT [PK_productInventory] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
