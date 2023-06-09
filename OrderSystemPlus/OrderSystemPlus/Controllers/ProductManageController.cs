1555b236c986a892d6b55ef9241c7b89aba71ea0
Yu Hsiang <umworksite@gmail.com>
2023-06-09 17:01:42 +0800

    Refactor Product Controller

--- OrderSystemPlus/OrderSystemPlus/Controllers/ProductManageController.cs
+++ OrderSystemPlus/OrderSystemPlus/Controllers/ProductManageController.cs
@@ -1,11 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 
+using OrderSystemPlus.BusinessActor;
+using OrderSystemPlus.Models.BusinessActor;
+
 namespace OrderSystemPlus.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class ProductManageController : ControllerBase
     {
-      
+        private readonly IProductManageHandler _productHandler;
+
+        public ProductManageController(
+            IProductManageHandler productHandler)
+        {
+            _productHandler = productHandler;
+        }
+
+        [HttpPost("CreateProduct")]
+        public async Task<IActionResult> CreateProduct([FromBody] List<ReqCreateProduct> req)
+        {
+            await _productHandler.HandleAsync(req);
+            return StatusCode(200);
+        }
+
+        [HttpPost("DeleteProduct")]
+        public async Task<IActionResult> DeleteProduct([FromBody] List<ReqDeleteProduct> req)
+        {
+            await _productHandler.HandleAsync(req);
+            return StatusCode(200);
+        }
+
+        [HttpPost("UpdateProduct")]
+        public async Task<IActionResult> UpdateProduct([FromBody] List<ReqUpdateProduct> req)
+        {
+            await _productHandler.HandleAsync(req);
+            return StatusCode(200);
+        }
+
+        [HttpPost("GetProductList")]
+        public async Task<List<RspGetProductList>> GetProductList([FromBody] ReqGetProductList req)
+            => await _productHandler.GetProductListAsync(req);
     }
 }
