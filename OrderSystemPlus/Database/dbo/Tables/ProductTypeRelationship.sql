19e0255dbe14aa1d57903dddb8e0168620c0e2ff
yuhsiang237 <umworksite@gmail.com>
2023-06-13 19:34:56 +0800

    Adjust table

---
+++ OrderSystemPlus/Database/dbo/Tables/ProductTypeRelationship.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProductTypeRelationship] (
+    [Id]                INT IDENTITY (1, 1) NOT NULL,
+    [ProductId]         INT NULL,
+    [ProductTypeId] INT NULL,
+    CONSTRAINT [PK_ProductTypeRelationship] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
