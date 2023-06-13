35a09432be67d550bb1356e68f488a40d3e0cc8d
yuhsiang237 <umworksite@gmail.com>
2023-06-13 19:57:47 +0800

    Add ProductTypeRelationshipRepository WIP

---
+++ OrderSystemPlus/OrderSystemPlus/DataAccessor/_ProductTypeRelationship/ProductTypeRelationshipRepository.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+
+using Dapper;
+
+using OrderSystemPlus.Models;
+using OrderSystemPlus.Models.DataAccessor;
+
+namespace OrderSystemPlus.DataAccessor
+{
+    public class ProductTypeRelationshipRepository : IProductTypeRelationshipRepository
+    {
+        public async Task<List<ProductTypeRelationshipDto>> FindByOptionsAsync(int? productId = null, int? productTypeId = null)
+        {
+
+            string sql = @"
+                           SELECT
+                                [ProductId],
+                                [ProductTypeId]
+                           FROM [dbo].[ProductTypeRelationship]";
+
+            var conditions = new List<string>
+            {
+            };
+
+            if (productId.HasValue)
+                conditions.Add("[ProductId] = @ProductId");
+            if (productTypeId.HasValue)
+                conditions.Add("[ProductTypeId] = @ProductTypeId");
+
+            if (conditions.Any())
+                sql = string.Concat(sql, $" WHERE {string.Join(" AND ", conditions)}");
+
+            var result = default(List<ProductTypeRelationshipDto>);
+            using (SqlConnection conn = new SqlConnection(DBConnection.GetConnectionString()))
+            {
+                result = (await conn.QueryAsync<ProductTypeRelationshipDto>(sql, new
+                {
+                    ProductId = productId,
+                    ProductTypeId = productTypeId,
+                })).ToList();
+            }
+
+            return result;
+        }
+        public async Task<List<int>> InsertAsync(IEnumerable<ProductTypeRelationshipDto> model)
+        {
+            var result = new List<int>();
+            using (var ts = new TransactionScope())
+            {
+                using (SqlConnection conn = new SqlConnection(DBConnection.GetConnectionString()))
+                {
+                    result = model
+                        .Select(s => InsertAsync(s, conn))
+                        .ToList();
+
+                    if (!result.Any(a => a == 0))
+                        ts.Complete();
+                }
+            }
+
+            return await Task.FromResult(result);
+        }
+
+        private int InsertAsync(ProductTypeRelationshipDto command, IDbConnection cn)
+        {
+            var sql = @"
+                INSERT INTO [dbo].[ProductTypeRelationship]
+                (
+                    [ProductId],
+                    [ProductTypeId]
+                ) VALUES
+                (
+                    @ProductId,
+                    @ProductTypeId
+                )
+                SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY];
+                ";
+            return cn.ExecuteScalar<int>(sql, command);
+        }
+
+        public async Task DeleteAsync(ProductTypeRelationshipDto model)
+        {
+            var sql = @"
+                UPDATE [dbo].[ProductTypeRelationship]
+                SET 
+                    [IsValid] = 0,
+                    [UpdatedOn] = @UpdatedOn
+
+                ";
+            var conditions = new List<string>
+            {
+            };
+
+            if (model.ProductId.HasValue)
+                conditions.Add("[ProductId] = @ProductId");
+            if (model.ProductTypeId.HasValue)
+                conditions.Add("[ProductTypeId] = @ProductTypeId");
+
+            if (conditions.Any())
+                sql = string.Concat(sql, $" WHERE {string.Join(" AND ", conditions)}");
+
+            using (SqlConnection conn = new SqlConnection(DBConnection.GetConnectionString()))
+            {
+                await conn.ExecuteAsync(sql, model);
+            }
+        }
+    }
+}
---
+++ OrderSystemPlus/OrderSystemPlus/DataAccessor/_ProductTypeRelationship/_/IProductTypeRelationshipRepository.cs
@@ -0,0 +1,26 @@
+﻿using OrderSystemPlus.Models.DataAccessor;
+
+namespace OrderSystemPlus.DataAccessor
+{
+    public interface IProductTypeRelationshipRepository
+    {
+        /// <summary>
+        /// 刪除ProductTypeRelationship資料們
+        /// </summary>
+        /// <param name="commands"></param>
+        /// <returns></returns>
+        Task DeleteAsync(ProductTypeRelationshipDto model);
+
+        /// <summary>
+        /// 新增ProductTypeRelationship資料們
+        /// </summary>
+        /// <param name="commands"></param>
+        /// <returns></returns>
+        Task<List<int>> InsertAsync(IEnumerable<ProductTypeRelationshipDto> model);
+
+        /// <summary>
+        /// 查詢ProductTypeRelationship資料們
+        /// </summary>
+        /// <returns></returns>
+        Task<List<ProductTypeRelationshipDto>> FindByOptionsAsync(int? productId = null, int? productTypeId = null);   }
+}
