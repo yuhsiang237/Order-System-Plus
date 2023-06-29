f494204fc76b7728021aca2206dce4ec7eaa90b4
Yu Hsiang <umworksite@gmail.com>
2023-06-29 14:19:43 +0800

    #41 Adjust table and add Dto model

--- OrderSystemPlus/Database/dbo/Tables/ReturnShipmentOrderDetail.sql
+++ OrderSystemPlus/Database/dbo/Tables/ReturnShipmentOrderDetail.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE [dbo].[ReturnShipmentOrderDetail] (
-    [Id]              INT IDENTITY(1,1) NOT NULL,
-    [ReturnShipmentOrderNumber]     VARCHAR (100)   NOT NULL,
-    [ShipmentOrderDetailId]     INT   NOT NULL,
-    [ProductId]       INT             NULL,
-    [ProductQuantity] DECIMAL (18, 4) NULL,
-    [Remarks]         NVARCHAR (500)  NULL,
-    [CreatedOn]       DATETIME        NOT NULL,
-    [UpdatedOn]       DATETIME        NOT NULL,
-    [IsValid]         BIT             NOT NULL, 
-    CONSTRAINT [PK_ReturnShipmentOrderDetail] PRIMARY KEY ([Id])
+    [Id]                        INT             IDENTITY (1, 1) NOT NULL,
+    [ReturnShipmentOrderNumber] VARCHAR (100)   NOT NULL,
+    [ShipmentOrderDetailId]     INT             NOT NULL,
+    [ReturnProductQuantity]     DECIMAL (18, 4) NULL,
+    [Remarks]                   NVARCHAR (500)  NULL,
+    [CreatedOn]                 DATETIME        NOT NULL,
+    [UpdatedOn]                 DATETIME        NOT NULL,
+    [IsValid]                   BIT             NOT NULL,
+    CONSTRAINT [PK_ReturnShipmentOrderDetail] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
@@ -34,23 +35,9 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level2name = N'Id'
 GO
 
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'產品ID',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'ReturnShipmentOrderDetail',
-    @level2type = N'COLUMN',
-    @level2name = N'ProductId'
+
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'退貨產品數量',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'ReturnShipmentOrderDetail',
-    @level2type = N'COLUMN',
-    @level2name = N'ProductQuantity'
+
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
     @value = N'備註',
@@ -96,3 +83,6 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'ReturnShipmentOrderDetail',
     @level2type = N'COLUMN',
     @level2name = N'ShipmentOrderDetailId'
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'退貨產品數量', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ReturnShipmentOrderDetail', @level2type = N'COLUMN', @level2name = N'ReturnProductQuantity';
+
