d6bb9a7fa4d0895ee2273c8ba18931eaf1aaef28
Yu Hsiang <umworksite@gmail.com>
2023-07-06 10:51:42 +0800

    #41 Add unit test for ReturnShipmentOrderUpdate

--- OrderSystemPlus/OrderSystemPlusTest/BusinessActor/_ReturnShipmentOrderManage/ReturnShipmentOrderManageHandlerTest.cs
+++ OrderSystemPlus/OrderSystemPlusTest/BusinessActor/_ReturnShipmentOrderManage/ReturnShipmentOrderManageHandlerTest.cs
@@ -29,10 +29,10 @@ namespace OrderSystemPlusTest.BusinessActor
               _productInventoryHandler.Object);
         }
 
-        [Fact]
-        public async Task ReturnShipmentOrderCreate()
+        [Theory]
+        [InlineData("R202306277890DB", "S202306277890DB")]
+        public async Task ReturnShipmentOrderCreate(string returnShipmentOrderNumber, string shipmentOrderNumber)
         {
-            var shipmentOrderNumber = "S202306277890DB";
             _shipmentOrderRepository
                 .Setup(x => x.FindByOptionsAsync(It.IsAny<string?>()))
                 .ReturnsAsync(new List<ShipmentOrderDto> {
@@ -64,12 +64,16 @@ namespace OrderSystemPlusTest.BusinessActor
             _returnShipmentOrderRepository
              .Setup(x => x.FindByOptionsAsync(It.IsAny<string?>(), It.IsAny<string?>()))
              .ReturnsAsync(new List<ReturnShipmentOrderDto> { });
-            
+
             _productInventoryHandler.Setup(x => x.HandleAsync(It.IsAny<List<ReqUpdateProductInventory>>())).ReturnsAsync(true);
+            _returnShipmentOrderRepository.Setup(x => x.InsertAsync(It.IsAny<IEnumerable<ReturnShipmentOrderDto>>())).ReturnsAsync(new List<string>
+            {
+                returnShipmentOrderNumber
+            });
 
             await _handler.HandleAsync(new ReqCreateReturnShipmentOrder
             {
-                ShipmentOrderNumber = "S202306277890DB",
+                ShipmentOrderNumber = shipmentOrderNumber,
                 ReturnDate = DateTime.Now,
                 Remark = "test",
                 Details = new List<ReqCreateReturnShipmentOrder.ReturnShipmentOrderDetail2>
@@ -83,6 +87,7 @@ namespace OrderSystemPlusTest.BusinessActor
                 }
             });
 
+            _returnShipmentOrderRepository.Verify(x => x.InsertAsync(It.IsAny<IEnumerable<ReturnShipmentOrderDto>>()), Times.Once());
             _productInventoryHandler.Verify(x => x.HandleAsync(It.IsAny<List<ReqUpdateProductInventory>>()), Times.Once());
             _shipmentOrderRepository.Verify(x => x.FindByOptionsAsync(It.IsAny<string?>()), Times.Once());
             _returnShipmentOrderRepository
@@ -113,10 +118,86 @@ namespace OrderSystemPlusTest.BusinessActor
             _returnShipmentOrderRepository.Verify(x => x.FindByOptionsAsync(It.IsAny<string?>(), It.IsAny<string?>()), Times.Once());
         }
 
-        [Fact]
-        public async Task ReturnShipmentOrderUpdate()
+        [Theory]
+        [InlineData("R202306277890DB", "S202306277890DB")]
+        public async Task ReturnShipmentOrderUpdate(string returnShipmentOrderNumber, string shipmentOrderNumber)
         {
-            // TODO
+            _returnShipmentOrderRepository
+             .Setup(x => x.FindByOptionsAsync(It.IsAny<string?>(), It.IsAny<string?>()))
+             .ReturnsAsync(new List<ReturnShipmentOrderDto> {
+                 new ReturnShipmentOrderDto{
+                     ReturnShipmentOrderNumber = returnShipmentOrderNumber,
+                     ShipmentOrderNumber = shipmentOrderNumber,
+                     TotalReturnAmount = 0,
+                     ReturnDate = DateTime.Now,
+                     Remark = "Test",
+                     OperatorUserId = 999,
+                     Details = new List<ReturnShipmentOrderDetailDto>
+                     {
+                         new ReturnShipmentOrderDetailDto
+                         {
+                             Id = 10,
+                             ReturnShipmentOrderNumber = returnShipmentOrderNumber,
+                             ShipmentOrderDetailId  = 10,
+                             ReturnProductQuantity = 0,
+                             Remarks = "Test",
+                         }
+                     }
+                 }
+             });
+
+            _shipmentOrderRepository
+                .Setup(x => x.FindByOptionsAsync(It.IsAny<string?>()))
+                .ReturnsAsync(new List<ShipmentOrderDto> {
+                    new ShipmentOrderDto
+                    {
+                        OrderNumber = shipmentOrderNumber,
+                        TotalAmount = 100,
+                        RecipientName = "user",
+                        OperatorUserId = 123,
+                        Status = OrderSystemPlus.Enums.ShipmentOrderStatus.Finish,
+                        FinishDate = DateTime.Now,
+                        DeliveryDate = DateTime.Now,
+                        Address = "test",
+                        Details = new List<ShipmentOrderDetailDto>
+                        {
+                            new ShipmentOrderDetailDto
+                            {
+                                Id = 10,
+                                OrderNumber = shipmentOrderNumber,
+                                ProductId = 79,
+                                ProductNumber = "T1N",
+                                ProductPrice = 100,
+                                ProductQuantity = 1,
+                            }
+                        }
+                    }
+                });
+
+            _productInventoryHandler.Setup(x => x.HandleAsync(It.IsAny<List<ReqUpdateProductInventory>>())).ReturnsAsync(true);
+
+            _returnShipmentOrderRepository.Setup(x => x.UpdateAsync(It.IsAny<IEnumerable<ReturnShipmentOrderDto>>()));
+
+            await _handler.HandleAsync(new ReqUpdateReturnShipmentOrder
+            {
+                ReturnShipmentOrderNumber = returnShipmentOrderNumber,
+                ReturnDate = DateTime.Now,
+                Remark = "Test",
+                Details = new List<ReqUpdateReturnShipmentOrder.ReturnShipmentOrderDetail3>
+                {
+                    new ReqUpdateReturnShipmentOrder.ReturnShipmentOrderDetail3
+                    {
+                        Id = 10,
+                        ReturnProductQuantity = 1,
+                        Remarks = "test"
+                    }
+                }
+            });
+
+            _productInventoryHandler.Verify(x => x.HandleAsync(It.IsAny<List<ReqUpdateProductInventory>>()), Times.Once());
+            _returnShipmentOrderRepository.Verify(x => x.FindByOptionsAsync(It.IsAny<string?>(), It.IsAny<string?>()), Times.Once());
+            _shipmentOrderRepository.Verify(x => x.FindByOptionsAsync(It.IsAny<string?>()), Times.Once());
+            _returnShipmentOrderRepository.Verify(x => x.UpdateAsync(It.IsAny<IEnumerable<ReturnShipmentOrderDto>>()), Times.Once());
         }
 
         [Fact]
