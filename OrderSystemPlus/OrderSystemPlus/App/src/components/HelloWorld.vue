1fb10dd58e3643755ae0f213754637e602eea2f4
Yu Hsiang <umworksite@gmail.com>
2023-07-11 10:57:19 +0800

    #46 Adjust vue base

--- OrderSystemPlus/OrderSystemPlus/App/src/components/HelloWorld.vue
+++ OrderSystemPlus/OrderSystemPlus/App/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string;
-}>();
+  msg: string
+}>()
 </script>
 
 <template>
@@ -10,8 +10,7 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
 </template>
@@ -20,6 +19,7 @@ defineProps<{
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
+  position: relative;
   top: -10px;
 }
 
--- OrderSystemPlus/OrderSystemPlus/App/vite.config.ts
+++ OrderSystemPlus/OrderSystemPlus/App/vite.config.ts
@@ -1,17 +1,22 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5000
+    port: 3030
   },
-  plugins: [vue()],
+  preview: {
+    port: 8080
+  },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
