2ebe919e8e64e81b34f25ae036f0dbef6b2dbd35
Yu Hsiang <umworksite@gmail.com>
2023-07-12 14:18:15 +0800

    #49 adjust port

--- OrderSystemPlus/OrderSystemPlus/App/vite.config.ts
+++ OrderSystemPlus/OrderSystemPlus/App/vite.config.ts
@@ -6,10 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3030
-  },
-  preview: {
-    port: 8080
+    port: 3000
   },
   plugins: [
     vue(),
